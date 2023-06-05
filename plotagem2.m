99a2981cb97cecca19b90998aaedf8167ba2583b
Lucas-b.l.u.e <114190156+Deusmiseravel@users.noreply.github.com>
2023-06-05 13:58:35 -0300

    plotagem2.m

--- plotagem2.m
+++ plotagem2.m
@@ -1,9 +1,3 @@
-%% 
-%     Curso do canal ExataMenteS
-%     Aula 5 - Produto Escalar e Plot 2d/3d de vetores
-%     Se inscreva no canal e nos ajude a crescer <3
-%     Vamos melhorar o Brasil atráves da tecnologia :)
-%     Instrutor: João Pinheiro
 
 % Plot 2D
 % Metodo I
